1f0e3305996131468bef5fc09dd6c6327f7d2ea4
hitom <hitomi.hoshino@gmail.com>
2021-01-11 15:22:56 +0100

    Changed names and added class

--- Nettbank/src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
+++ Nettbank/src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
@@ -21,7 +21,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class EnhetstestAdminController {
+public class EnhetstestAdminKontoController {
 
     @InjectMocks
     // denne skal testes
