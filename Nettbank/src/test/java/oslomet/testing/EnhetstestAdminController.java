12d24aecf9e4df0f4b21bb77344c8b0badad8c45
hitom <hitomi.hoshino@gmail.com>
2021-01-11 15:22:10 +0100

    Changed names and added class

--- Nettbank/src/test/java/oslomet/testing/EnhetstestAdminController.java
+++ Nettbank/src/test/java/oslomet/testing/EnhetstestAdminController.java
@@ -1,4 +1,37 @@
 package oslomet.testing;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import oslomet.testing.API.AdminKontoController;
+import oslomet.testing.API.BankController;
+import oslomet.testing.DAL.BankRepository;
+import oslomet.testing.Models.Konto;
+import oslomet.testing.Models.Kunde;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class EnhetstestAdminController {
+
+    @InjectMocks
+    // denne skal testes
+    private AdminKontoController adminKontoController;
+
+    @Mock
+    // denne skal Mock'es
+    private BankRepository repository;
+
+    @Mock
+    // denne skal Mock'es
+    private Sikkerhet sjekk;
 }
