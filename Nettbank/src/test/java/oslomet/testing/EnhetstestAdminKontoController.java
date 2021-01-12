dda1e7a634e8314eff8deddcbe275e765bc3e482
Maja Tengstedt <maja.tengstedt@gmail.com>
2021-01-12 14:33:46 +0100

    Konto hentAllKontiOK is coded

--- Nettbank/src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
+++ Nettbank/src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
@@ -20,6 +20,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -38,5 +39,117 @@ public class EnhetstestAdminKontoController {
     // denne skal Mock'es
     private Sikkerhet sjekk;
 
+    @Test
+
+    public void hentAllKontiOK(){
+
+        // arrage
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+        List<Konto> kontolist = new ArrayList<>();
+        kontolist.add(konto1);
+        kontolist.add(konto2);
+
+        when(sjekk.loggetInn()).thenReturn("11111111111");
+        Mockito.when(repository.hentAlleKonti()).thenReturn(kontolist);
+
+        // act
+        List<Konto> resultat = adminKontoController.hentAlleKonti();
+
+        // assert
+        assertEquals(kontolist, resultat);
+    }
+
+   /* @Test
+    public void hentAlleFeil() {
+
+        // arrage
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        List<Kunde> resultat = adminKontoController.hentAlle();
+
+        // assert
+        assertNull(resultat);
+    }
+
+
+    @Test
+    public void lagreKundeOK(){
+
+        Kunde kunde = new Kunde();
+
+        when(sjekk.loggetInn()).thenReturn("11111111111");
+
+        Mockito.when(repository.registrerKunde(any(Kunde.class))).thenReturn("OK");
+
+        String resultat = adminKontoController.lagreKunde(kunde);
+
+        assertEquals("OK", resultat);
+
+    }
+
+    @Test
+    public void lagreKundeFeil(){
+
+        Kunde kunde = new Kunde();
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        String resultat = adminKontoController.lagreKunde(kunde);
+
+        assertEquals("Ikke logget inn", resultat);
+
+    }
+
+    @Test
+    public void endreOK(){
+
+        Kunde kunde = new Kunde();
+
+        when(sjekk.loggetInn()).thenReturn("11111111111");
+
+        Mockito.when(repository.endreKundeInfo(any(Kunde.class))).thenReturn("OK");
+
+        String restulat = adminKontoController.endre(kunde);
+
+        assertEquals("OK", restulat);
+    }
+
+    @Test
+    public void endreFeil() {
+
+        Kunde kunde = new Kunde();
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        String restulat = adminKontoController.endre(kunde);
+
+        assertEquals("Ikke logget inn", restulat);
+    }
+
+    @Test
+    public void slettOK(){
+
+        when(sjekk.loggetInn()).thenReturn("11111111111");
+
+        Mockito.when(repository.slettKunde(any())).thenReturn("OK");
+
+        String restulat = adminKundeController.slett("01010110523");
+
+        assertEquals("OK", restulat);
+    }
+
+    @Test
+    public void slettFeil() {
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        String restulat = adminKundeController.slett("01010110523");
+
+        assertEquals("Ikke logget inn", restulat);
+    }
+
+    */
 
 }
