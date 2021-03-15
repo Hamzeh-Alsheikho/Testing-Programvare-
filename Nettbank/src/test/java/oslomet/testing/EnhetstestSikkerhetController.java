60b90c3a349b974f9a6f2a721f017103ba49e577
Hamzeh0086 <s339572@oslomet.no>
2021-03-15 11:48:27 +0100

    Ferdig med enhatesTesten

--- Nettbank/src/test/java/oslomet/testing/EnhetstestSikkerhetController.java
+++ Nettbank/src/test/java/oslomet/testing/EnhetstestSikkerhetController.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -31,12 +32,12 @@ public class EnhetstestSikkerhetController {
     private AdminRepository adminRepository;
 
     @Mock
-    MockHttpSession session;
+    private MockHttpSession session;
 
-    @Before
-    public void initSession() {
+   @Test
+    public void test_loggetInn() {
 
-        Map<String, Object> attributes = new HashMap<String, Object>();
+    Map<String, Object> attributes = new HashMap<String, Object>();
 
         doAnswer(new Answer<Object>() {
             @Override
@@ -56,89 +57,77 @@ public class EnhetstestSikkerhetController {
             }
         }).when(session).setAttribute(anyString(), any());
 
+        session.setAttribute("Innlogget","Innlogget");
+
+       String resultat = sikkerhetContoller.loggetInn();
+       // assert
+       assertEquals("Innlogget",resultat);
+
+    }
+    @Test
+    public void test_loggetInn_Feil() {
+
+    Map<String, Object> attributes = new HashMap<String, Object>();
+
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                String key = (String) invocation.getArguments()[0];
+                return attributes.get(key);
+            }
+        }).when(session).getAttribute(anyString());
+
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                String key = (String) invocation.getArguments()[0];
+                Object value = invocation.getArguments()[1];
+                attributes.put(key, value);
+                return null;
+            }
+        }).when(session).setAttribute(anyString(), any());
+
+        session.setAttribute("Innlogget",null);
+
+       String resultat = sikkerhetContoller.loggetInn();
+       // assert
+       assertNull(resultat);
     }
 
     @Test
     public void test_sjekkLoggetInn() {
-        // arrange
         when(bankRepository.sjekkLoggInn(anyString(), anyString())).thenReturn("OK");
-
-        // setningen under setter ikke attributten, dvs. at det ikke er mulig å sette en attributt i dette oppsettet
-        session.setAttribute("Innlogget", "12345678901");
-
-        // act
         String resultat = sikkerhetContoller.sjekkLoggInn("12345678901", "HeiHeiHei");
-        // assert
         assertEquals("OK", resultat);
     }
     @Test
     public void test_sjekkLoggetInn_FeilPersonNummer() {
-        // arrange
-        when(bankRepository.sjekkLoggInn(anyString(), anyString())).thenReturn("OK");
-
-        // setningen under setter ikke attributten, dvs. at det ikke er mulig å sette en attributt i dette oppsettet
-        session.setAttribute("Innlogget", "123456789012");
-
-        // act
         String resultat = sikkerhetContoller.sjekkLoggInn("123456789012", "HeiHeiHei");
-        // assert
         assertEquals("Feil i personnummer", resultat);
     }
 
     @Test
     public void test_sjekkLoggetInn_FeilPassword() {
-        // arrange
-        when(bankRepository.sjekkLoggInn(anyString(), anyString())).thenReturn("OK");
-
-        // setningen under setter ikke attributten, dvs. at det ikke er mulig å sette en attributt i dette oppsettet
-        session.setAttribute("Innlogget", "12345678901");
-
-        // act
         String resultat = sikkerhetContoller.sjekkLoggInn("12345678901", "Hei");
-        // assert
         assertEquals("Feil i passord", resultat);
     }
 
 
     @Test
     public void test_loggInnAdmin_OK() {
-        // arrange
-        when(bankRepository.sjekkLoggInn(anyString(), anyString())).thenReturn("OK");
-
-        // setningen under setter ikke attributten, dvs. at det ikke er mulig å sette en attributt i dette oppsettet
-        session.setAttribute("Innlogget", "Admin");
-
-        // act
         String resultat = sikkerhetContoller.loggInnAdmin("Admin", "Admin");
-        // assert
         assertEquals("Logget inn", resultat);
     }
 
     @Test
     public void test_loggInn_FeilBrukerAdmin() {
-        // arrange
-        when(bankRepository.sjekkLoggInn(anyString(), anyString())).thenReturn("OK");
-
-        // setningen under setter ikke attributten, dvs. at det ikke er mulig å sette en attributt i dette oppsettet
-        session.setAttribute("Innlogget", "Admin");
-
-        // act
         String resultat = sikkerhetContoller.loggInnAdmin("admin", "Admin");
-        // assert
         assertEquals("Ikke logget inn", resultat);
     }
 
     @Test
     public void test_loggInn_FeilPasswordAdmin() {
-        // arrange
-        when(bankRepository.sjekkLoggInn(anyString(), anyString())).thenReturn("OK");
-
-        // setningen under setter ikke attributten, dvs. at det ikke er mulig å sette en attributt i dette oppsettet
-        session.setAttribute("Innlogget", "Admin");
-
-        // act
         String resultat = sikkerhetContoller.loggInnAdmin("Admin", "admin");
-        // assert
         assertEquals("Ikke logget inn", resultat);
     }
 }
